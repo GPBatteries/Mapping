e3182121cf88423b49e9c2b0b36edae226fda90d
GPBatteries <Pieter_Hollanders@goldpeak.com>
2026-07-10 09:56:47 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,17 @@
-export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDlWNgJQAO6H8RUyeEuDARqNx774MQMyGU",
+  authDomain: "mapping-ddc45.firebaseapp.com",
+  projectId: "mapping-ddc45",
+  storageBucket: "mapping-ddc45.firebasestorage.app",
+  messagingSenderId: "957709018632",
+  appId: "1:957709018632:web:e6750d8c3bda8a5487d3bd"
 };
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
