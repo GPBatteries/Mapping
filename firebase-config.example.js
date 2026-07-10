81fe0c1509906f8e1f6d15ca4b87544a54747ba0
GPBatteries <Pieter_Hollanders@goldpeak.com>
2026-07-10 11:40:25 +0200

    Delete firebase-config.example.js

--- firebase-config.example.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDlWNgJQAO6H8RUyeEuDARqNx774MQMyGU",
-  authDomain: "mapping-ddc45.firebaseapp.com",
-  projectId: "mapping-ddc45",
-  storageBucket: "mapping-ddc45.firebasestorage.app",
-  messagingSenderId: "957709018632",
-  appId: "1:957709018632:web:e6750d8c3bda8a5487d3bd",
-};
